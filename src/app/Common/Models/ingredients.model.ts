062874a64b50a8b5525570ea64445a40195d22b9
naveenk90 <45628286+naveenk90@users.noreply.github.com>
2022-09-14 13:22:28 +0530

    2 files modified, 2 added, 1 deleted

---
+++ src/app/Common/Models/ingredients.model.ts
@@ -0,0 +1,12 @@
+export class Ingredients {
+  pizzaCrustSize: string;
+  pizzaCrustSizePrice: string;
+  sauce: string;
+  saucePrice: string;
+  cheese: boolean;
+  cheesePrice: string;
+  extraCheese: boolean;
+  extraCheesePrice: string;
+  toppings: string;
+  toppingsPrice: string;
+}
---
+++ src/app/Common/Models/products.model.ts
@@ -0,0 +1,8 @@
+export class Products {
+  id: string;
+  type: string;
+  price: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
