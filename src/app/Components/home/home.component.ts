7b04eb3ab0fecdd72d47a518178cc38e470e6f65
naveenk90 <45628286+naveenk90@users.noreply.github.com>
2022-09-14 17:52:23 +0530

    3 files modified

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from '../../Common/Models/products.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  productsList: Products[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.bindProductsList();
   }
 
+  bindProductsList(): void {
+    this.productsList = [
+      {
+        id: '1',
+        type: 'pizza',
+        price: '120',
+        name: 'Chicken Tikka',
+        description: 'chicken tikka pizza with sauce',
+        imageUrl:
+          'https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/margherita.90f9451fd66871fb6f9cf7d506053f18.1.jpg?width=800',
+        ingredients: null,
+      },
+      {
+        id: '2',
+        type: 'pizza',
+        price: '120',
+        name: 'Margherita',
+        description: 'Margherita pizza with sauce',
+        imageUrl:
+          'https://api.pizzahut.io/v1/content/en-in/in-1/images/pizza/margherita.90f9451fd66871fb6f9cf7d506053f18.1.jpg?width=800',
+        ingredients: null,
+      },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule,CommonModule ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
